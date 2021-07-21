d8e5581330fcd2d5b5ac7fd923e6e134ef53622a
just331 <justinr331@gmail.com>
2021-07-20 22:28:20 -0500

    Updating header sections for each page.

--- app.py
+++ app.py
@@ -7,29 +7,28 @@ app = Flask(__name__)
 @app.route('/index', methods=['POST', 'GET'])
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    greeting = 'Welcome to My A.I. / Data Science Portfolio Website [Under Construction!]'
-
+    title_text = 'Welcome To My Data Science & Logic Programming Portfolio Website [Under Construction!] '
     return render_template('/index.html',
-                           greeting=greeting,
-                           title="Welcome to My Data Science & Logic Programming Portfolio Website [Under "
-                                 "Construction!]",
+                           title_text=title_text,
+                           title="Data Science and Logic Programming",
                            id="index")
 
 
 # About Me page
 @app.route('/about', methods=['POST', 'GET'])
 def about():
-    about_me = 'About me'
+    title_text = 'Recent Texas Tech University graduate with a Master\'s degree in Computer Science. Interests in ' \
+                 'Data Science and Logic Programming. Below is a brief timeline of my career so far. '
     return render_template('/about.html',
-                           about=about_me,
+                           title_text=title_text,
                            title="About Me",
                            id="about")
 
 
 @app.route('/portfolio', methods=['POST', 'GET'])
 def portfolio():
-    port = 'Portfolio Page'
+    title_text = 'Here are a few of my favorite projects that I have completed throughout my educational career.'
     return render_template('/portfolio.html',
-                           port=port,
+                           title_text=title_text,
                            title="Portfolio",
                            id="portfolio")
